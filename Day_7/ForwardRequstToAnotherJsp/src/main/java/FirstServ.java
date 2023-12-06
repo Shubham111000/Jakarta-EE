1c7947d6b3b139edc3d9b854235fd538a8969792
Shubham Gaikwad <74091710+Shubham111000@users.noreply.github.com>
2023-12-06 08:35:32 +0530

    Add files via upload

---
+++ Day_7/ForwardRequstToAnotherJsp/src/main/java/FirstServ.java
@@ -0,0 +1,28 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class FirstServ
+ */
+public class FirstServ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw=response.getWriter();
+		pw.println("Inside FirstServ");
+		pw.println("<br>");
+		pw.println(request.getParameter("role"));
+		}
+
+}

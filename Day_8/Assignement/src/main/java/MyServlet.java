ac661b0360e56e41a29fb698a6fd88be05fb634b
Shubham Gaikwad <74091710+Shubham111000@users.noreply.github.com>
2023-12-07 08:28:39 +0530

    Add files via upload

---
+++ Day_8/Assignement/src/main/java/MyServlet.java
@@ -0,0 +1,41 @@
+
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		List<String> list=new ArrayList<String>();
+		list.add("Sushil");
+		list.add("Ajinkya");
+		list.add("Akshay");
+		list.add("Yash");
+		HttpSession session=request.getSession();
+		session.setAttribute("list",list);
+		PrintWriter pw=response.getWriter();
+		pw.println("Session Created");
+		//RequestDispatcher rd=request.getRequestDispatcher("MyJsp.jsp");
+		//rd.forward(request,response);
+		
+	}
+
+}

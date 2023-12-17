bc427ee887fc9c22b504f8284d40a0f18a2ee2a9
Shubham Gaikwad <shubham.gaikwad1011@gmail.com>
2023-12-17 23:49:56 +0530

    .

---
+++ Day_17/Spring_Web_CURD_JPA/src/main/java/com/example/demo/SpringWebCurdJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebCurdJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebCurdJpaApplication.class, args);
+	}
+
+}

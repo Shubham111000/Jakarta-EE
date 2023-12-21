8f74d72fb7ddd3e000f82a43479b99489c99d22a
Shubham Gaikwad <shubham.gaikwad1011@gmail.com>
2023-12-21 09:33:34 +0530

    .

--- Day_19/UserServicePro/src/main/java/com/example/controllers/UserController.java
+++ Day_19/UserServicePro/src/main/java/com/example/controllers/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.client.RestTemplate;
 
 import com.example.entities.Contact;
 import com.example.entities.User;
@@ -17,12 +17,21 @@ public class UserController
 {
 	@Autowired
 	private UserService service;
-	
+	@Autowired
+	private RestTemplate template;
 	
 	@GetMapping("/user/{userid}")
 	public User getUser(@PathVariable("userid") int userid)
 	{
-		return service.getUser(userid);
+		User user=service.getUser(userid);
+		// RestTemplate helps you to invoke one service from another service
+		// the only drawback is here "localhost" is hardcoded
+		// so if there are thousands of services running on different
+		// servers it will become difficult to keep a track of 
+		// server names
+		List<?> contacts=template.getForObject("http://localhost:9003/contact/"+user.getId(), List.class);
+		user.setContacts(contacts);
+		return user;
 
 	}
 }
--- Day_19/UserServicePro/src/main/java/com/example/demo/UserServiceProApplication.java
+++ Day_19/UserServicePro/src/main/java/com/example/demo/UserServiceProApplication.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.example.*")
@@ -12,6 +13,12 @@ public class UserServiceProApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceProApplication.class, args);
+		
+	}
+	@Bean
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
 	}
 
 }
